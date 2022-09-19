165a1814dcd1de22f9b825ae795ff42cd1f69108
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-09-19 02:04:48 -0300

    creo un servidor con express.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log(`Hola nodecitooo!!`);
 
-const express = require (`exprees`);
+const express = require (`express`);
 
 const app = express();
+
+app.get(`/`, (req, res) => {
+    res.send(`Miren este es mi primer servidor creado con node.js y express.js je!`)
+})
 
 app.listen(8081, () => {console.log(`Servidor funcionando`)});
